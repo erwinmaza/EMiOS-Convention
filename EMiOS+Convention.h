bf2693dd18311bee8508f06985caff0c73253b72
Erwin Mazariegos <erwinmaza@gmail.com>
2013-09-30 18:03:28 -0500

    More UICollectionView methods

--- EMiOS+Convention.h
+++ EMiOS+Convention.h
@@ -3,28 +3,28 @@
 
 /*
  
- Copyright (c) 2012 eMaza Mobile. All rights reserved.
+	Copyright (c) 2012 eMaza Mobile. All rights reserved.
+
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  
- Permission is hereby granted, free of charge, to any person obtaining
- a copy of this software and associated documentation files (the
- "Software"), to deal in the Software without restriction, including
- without limitation the rights to use, copy, modify, merge, publish,
- distribute, sublicense, and/or sell copies of the Software, and to
- permit persons to whom the Software is furnished to do so, subject to
- the following conditions:
- 
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
- */
+*/
 
 
 
@@ -60,8 +60,15 @@
 @interface UICollectionView (EMiOS_Convention)
 
 	- (void)registerNibWithClass:(Class)class;
+	- (void)registerHeaderNibWithClass:(Class)class;
+	- (void)registerFooterNibWithClass:(Class)class;
+	- (void)registerHeaderWithClass:(Class)class;
+	- (void)registerFooterWithClass:(Class)class;
 	- (void)registerClass:(Class)class;
 	- (id)dequeueCellWithClass:(Class)class forIndexPath:(NSIndexPath*)indexPath;
+	- (id)dequeueHeaderWithClass:(Class)class forIndexPath:(NSIndexPath*)indexPath;
+	- (id)dequeueFooterWithClass:(Class)class forIndexPath:(NSIndexPath*)indexPath;
+	- (UICollectionReusableView*)getFooterForPath:(NSIndexPath*)path;
 
 @end
 
--- EMiOS+Convention.m
+++ EMiOS+Convention.m
@@ -3,28 +3,28 @@
 
 /*
  
- Copyright (c) 2012 eMaza Mobile. All rights reserved.
+	Copyright (c) 2012 eMaza Mobile. All rights reserved.
+
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  
- Permission is hereby granted, free of charge, to any person obtaining
- a copy of this software and associated documentation files (the
- "Software"), to deal in the Software without restriction, including
- without limitation the rights to use, copy, modify, merge, publish,
- distribute, sublicense, and/or sell copies of the Software, and to
- permit persons to whom the Software is furnished to do so, subject to
- the following conditions:
- 
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
- */
+*/
 
 
 #import "EMiOS+Convention.h"
@@ -98,6 +98,22 @@
 	[self registerNib:[UINib nibWithNibName:NSStringFromClass(class) bundle:[NSBundle mainBundle]] forCellWithReuseIdentifier:NSStringFromClass(class)];
 }
 
+- (void)registerHeaderNibWithClass:(Class)class {
+	[self registerNib:[UINib nibWithNibName:NSStringFromClass(class) bundle:[NSBundle mainBundle]] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:NSStringFromClass(class)];
+}
+
+- (void)registerFooterNibWithClass:(Class)class {
+	[self registerNib:[UINib nibWithNibName:NSStringFromClass(class) bundle:[NSBundle mainBundle]] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:NSStringFromClass(class)];
+}
+
+- (void)registerHeaderWithClass:(Class)class {
+	[self registerClass:class forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:NSStringFromClass(class)];
+}
+
+- (void)registerFooterWithClass:(Class)class {
+	[self registerClass:class forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:NSStringFromClass(class)];
+}
+
 - (void)registerClass:(Class)class {
 	[self registerClass:class forCellWithReuseIdentifier:NSStringFromClass(class)];
 }
@@ -105,5 +121,20 @@
 - (id)dequeueCellWithClass:(Class)class forIndexPath:(NSIndexPath*)indexPath {
 	return [self dequeueReusableCellWithReuseIdentifier:NSStringFromClass(class) forIndexPath:indexPath];
 }
+
+- (id)dequeueHeaderWithClass:(Class)class forIndexPath:(NSIndexPath*)indexPath {
+	return [self dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:NSStringFromClass(class) forIndexPath:indexPath];
+}
+
+- (id)dequeueFooterWithClass:(Class)class forIndexPath:(NSIndexPath*)indexPath {
+	return [self dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:NSStringFromClass(class) forIndexPath:indexPath];
+}
+
+- (UICollectionReusableView*)getFooterForPath:(NSIndexPath*)path {
+	return [[self dataSource] collectionView:self viewForSupplementaryElementOfKind:UICollectionElementKindSectionFooter atIndexPath:path];
+}
+
+
+
 
 @end
